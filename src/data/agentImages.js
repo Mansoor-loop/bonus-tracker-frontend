958e18c96a180835c3385d0dbcd5a7f11e0f6fbc
Mansoor <mansoor.loop@gmail.com>
2026-01-22 18:17:45 -0500

    Agent Images update verraricka donvan...

--- src/data/agentImages.js
+++ src/data/agentImages.js
@@ -1,35 +1,36 @@
 // frontend/src/data/qualifierImages.js
 
 import stephanie from "../assests/agents/stephanie.png" ;
-import mozell from  "../assests/agents/MozellHardy.png"  ;
+import mozell from  "../assests/agents/Mozellblue.png"  ;
 import catherine from  "../assests/agents/Catherineanimate.png"  ;
-import kevin from  "../assests/agents/Kevin.png"  ;
-import donovan from  "../assests/agents/stephanie.png"  ;
-import robert from  "../assests/agents/Robert.png"  ;
-import johnb from  "../assests/agents/Johnb.png"  ;
+import kevin from  "../assests/agents/Kevinblue.png"  ;
+import donovan from  "../assests/agents/donovanblue.png"  ;
+import robert from  "../assests/agents/robertgreen.png"  ;
+import johnb from  "../assests/agents/johngreen.png"  ;
 import chase from  "../assests/agents/Chase.png"  ;
 import loren from  "../assests/agents/Lorenmurray.png"  ;
-import tia from  "../assests/agents/tia spice.png"  ;
-import mustafa from  "../assests/agents/Mustafa.png"  ;
-
+import tia from  "../assests/agents/Tiagreen.png"  ;
+import mustafa from  "../assests/agents/Mustafablue.png"  ;
+import leader from  "../assests/agents/leader.png"  ;
+import verraricka from  "../assests/agents/verrackia.png"  ;
 // fallback image
 import placeholder from  "../assests/agents/shadow.png" ;
 
 export const QUALIFIER_IMAGE_MAP = {
   "MOZELL HARDY": mozell,
   "STEPHANIE SANTIAGO": stephanie,
-  "MICHAEL LEADER": placeholder,
+  "MICHAEL LEADER": leader,
   "JOHN BARNES": johnb,
-  "Donovn Romario": donovan,
+  "DONOVN ROMARIO": donovan,
   "CATHERINE CASSESE": catherine,
   "TIA SPICER": tia,
   "CHASE DUROUSSEAU": chase,
-  "Robert Frater": robert,
-  "Loren Murray": loren,
-  "Mustafa Puello": mustafa,
-  "Verraricka Hosea": donovan,
+  "ROBERT FRATER": robert,
+  "LOREN MURRAY": loren,
+  "MUSTAFA PUELLO": mustafa,
+  "VERRARICKA HOSEA": verraricka,
   "KEVIN GONCALVES": kevin,
-  "Ron Maselko": donovan,
+  "RON MASELKO": placeholder,
 
 };
 
